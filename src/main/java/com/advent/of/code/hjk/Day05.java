aa5acbf8d6e2abb9e5e3b702e53a68a959d36af6
HJK <han.junkwang@gmail.com>
2021-11-16 01:28:27 +0800

    Day 5 part 2.

--- src/main/java/com/advent/of/code/hjk/Day05.java
+++ src/main/java/com/advent/of/code/hjk/Day05.java
@@ -2,6 +2,9 @@ package com.advent.of.code.hjk;
 
 import org.apache.commons.codec.digest.DigestUtils;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -12,15 +15,26 @@ public final class Day05 {
     }
 
     public static String part1(String input) {
-        return generate(input);
-    }
-
-    private static String generate(String prefix) {
         return IntStream.iterate(0, i -> i + 1)
-                .mapToObj(i -> DigestUtils.md5Hex(prefix + i))
+                .mapToObj(i -> DigestUtils.md5Hex(input + i))
                 .filter(v -> v.startsWith("00000"))
                 .limit(8)
                 .map(v -> String.valueOf(v.charAt(5)))
                 .collect(Collectors.joining());
+    }
+
+    public static String part2(String input) {
+        var password = new ArrayList<>(Collections.nCopies(8, " "));
+        var pattern = Pattern.compile("^00000[0-7]");
+        for (long i = 0; password.contains(" "); i++) {
+            var md5 = DigestUtils.md5Hex(input + i);
+            if (pattern.matcher(md5).find()) {
+                var index = Integer.parseInt(String.valueOf(md5.charAt(5)));
+                if (password.get(index).equals(" ")) {
+                    password.set(index, String.valueOf(md5.charAt(6)));
+                }
+            }
+        }
+        return String.join("", password);
     }
 }
--- src/test/java/com/advent/of/code/hjk/Day05Test.java
+++ src/test/java/com/advent/of/code/hjk/Day05Test.java
@@ -10,4 +10,9 @@ public class Day05Test {
     public void part1() {
         assertThat(Day05.part1("uqwqemis")).isEqualTo("1a3099aa");
     }
+
+    @Test
+    public void part2() {
+        assertThat(Day05.part2("uqwqemis")).isEqualTo("694190cd");
+    }
 }
