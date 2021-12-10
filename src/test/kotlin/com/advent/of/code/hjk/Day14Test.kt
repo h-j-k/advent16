cc9d8d1ae6deba6a5f3ab07e3ce82daa460bc5db
HJK <han.junkwang@gmail.com>
2021-12-11 00:20:47 +0800

    Day 14 minor test refactoring.

--- src/test/kotlin/com/advent/of/code/hjk/Day14Test.kt
+++ src/test/kotlin/com/advent/of/code/hjk/Day14Test.kt
@@ -1,6 +1,6 @@
 package com.advent.of.code.hjk
 
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
@@ -8,12 +8,12 @@ class Day14Test {
 
     @Test
     fun part1() {
-        Assertions.assertThat(Day14.part1("zpqevtbw")).isEqualTo(16106)
+        assertThat(Day14.part1("zpqevtbw")).isEqualTo(16106)
     }
 
     @Disabled // 20 mins...!
     @Test
     fun part2() {
-        Assertions.assertThat(Day14.part2("zpqevtbw")).isEqualTo(22423)
+        assertThat(Day14.part2("zpqevtbw")).isEqualTo(22423)
     }
 }
