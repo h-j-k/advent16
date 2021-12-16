6d049cea05e5f928b40bc5aa7b72294e21ece3ac
HJK <han.junkwang@gmail.com>
2021-12-16 22:47:13 +0800

    Day 20.

--- src/main/kotlin/com/advent/of/code/hjk/Day20.kt
+++ src/main/kotlin/com/advent/of/code/hjk/Day20.kt
@@ -4,33 +4,21 @@ import kotlin.math.pow
 
 object Day20 {
 
-    private fun grouped(input: List<String>): List<IpDenyList> =
+    private fun process(input: List<String>): List<IpDenyList> =
         input.map { line -> line.split("-").let { (from, to) -> IpDenyList(from.toLong(), to.toLong()) } }
-            .sortedBy { it.from }
-            .runningReduce { prev, current ->
-                if (current.from <= prev.to + 1) IpDenyList(prev.from, prev.to.coerceAtLeast(current.to)) else current
+            .sortedBy { it.from }.fold(mutableListOf()) { acc, v ->
+                acc.also {
+                    if (it.isEmpty() || v.from > it.last().to + 1) it.add(v)
+                    else it[it.lastIndex] = IpDenyList(it.last().from, it.last().to.coerceAtLeast(v.to))
+                }
             }
 
-    fun part1(input: List<String>): Long {
-        val grouped = grouped(input)
-        var last = grouped[0]
-        for (g in grouped) {
-            if (g.from != last.from) return last.to + 1
-            last = g
-        }
-        return Long.MIN_VALUE
-    }
+    fun part1(input: List<String>) = process(input)[0].to + 1
 
-    fun part2(input: List<String>): Long {
-        val grouped = grouped(input)
-        var last = grouped[0]
-        var count = 0L
-        for (g in grouped) {
-            if (g.from != last.from) count += g.from - 1 - last.to
-            last = g
-        }
-        Int.MAX_VALUE
-        return count + (2.0.pow(32.0).toLong() - 1 - last.to)
+    fun part2(input: List<String>) = process(input).let {
+        it.drop(1).fold(it[0] to 0L) { (p, r), v ->
+            v to r + v.from - 1 - p.to
+        }.second + (2.0.pow(32.0).toLong() - 1 - it.last().to)
     }
 
     private data class IpDenyList(val from: Long, val to: Long)
