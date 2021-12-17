f815da4ca1cabb76b026e061e8caa70c8209c68e
HJK <han.junkwang@gmail.com>
2021-12-17 17:32:56 +0800

    Day 20 test refactoring.

--- src/test/kotlin/com/advent/of/code/hjk/Day20Test.kt
+++ src/test/kotlin/com/advent/of/code/hjk/Day20Test.kt
@@ -1,17 +1,18 @@
 package com.advent.of.code.hjk
 
 import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class Day20Test : TestBase() {
 
     @Test
     fun part1() {
-        Assertions.assertThat(Day20.part1(input)).isEqualTo(22887907)
+        assertThat(Day20.part1(input)).isEqualTo(22887907)
     }
 
     @Test
     fun part2() {
-        Assertions.assertThat(Day20.part2(input)).isEqualTo(109)
+        assertThat(Day20.part2(input)).isEqualTo(109)
     }
 }
